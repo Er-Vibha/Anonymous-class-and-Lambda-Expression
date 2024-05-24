f05f35d480ace03b5ce19af489b648d8e018137d
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2024-05-24 15:00:55 +0530

    Create ThreadDemo.java

---
+++ ThreadDemo.java
@@ -0,0 +1,17 @@
+public class ThreadDemo {
+    public static void main(String[] args) {
+Runnable thread1=()->{
+    for(int i=0; i<=10;i++){
+        System.out.println(i);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+};
+Thread t=new Thread(thread1);
+t.start();
+}
+    }
+
